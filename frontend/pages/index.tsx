82f300b128c0afd8fbd0d91635403cae6b5cb908
karan singh <bugthug404@gmail.com>
2023-12-28 12:20:41 +0530

    index added back

---
+++ frontend/pages/index.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+import React from "react";
+
+export default function Index() {
+  return (
+    <div className="h-screen w-full flex justify-center items-center">
+      <div className="p-4 text-center">
+        <div className="mb-4 text-2xl font-semibold">
+          Groceries App (NodeJS + NextJs)
+        </div>
+        <div className="flex gap-4 justify-center">
+          <Link href={"/login"}>Login</Link>
+          <Link href={"/signup"}>Signup</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
